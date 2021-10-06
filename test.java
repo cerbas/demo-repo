97be2624bb0660b122315ae56efae1443c75e930
cerbas <cengiz_erbas@yahoo.com>
2021-10-06 23:38:22 +0300

    updated test.java

--- test.java
+++ test.java
@@ -1,7 +1,14 @@
 
 public class hello {
 
+	int id;
+
 	public static main(String[] args) {
 		System.out.println("Hello world!");
+		setID(5);
 	}
+
+	public int setID(int id) {
+		this.id = id;
+	}	
 }
